7c240f6b6b8b99286d4f1fcb263b3de90f2f5949
Nina <b410203039@gamil.com>
2023-02-23 11:14:02 +0800

    [example]

---
+++ vue-project/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+<div style>
+<H1>AG-Grid Example</H1>
+  <ag-grid-vue
+   style="width: 1000px; height: 500px"
+    class=" container ag-theme-alpine ag-grid"
+    :columnDefs="columnDefs"
+    :rowData="rowData"
+  >
+  </ag-grid-vue>
+</div>
+
+</template>
+
+<script>
+
+import { AgGridVue } from "ag-grid-vue3";
+import "ag-grid-community/styles/ag-grid.css"; // Core grid CSS, always needed
+import "ag-grid-community/styles/ag-theme-alpine.css";
+
+export default {
+  name: "App",
+  components: {
+    AgGridVue,
+  },
+  setup() {
+    return {
+      columnDefs: [
+        { headerName: "Make", field: "make" },
+        { headerName: "Model", field: "model" },
+        { headerName: "Price", field: "price" },
+      ],
+      rowData: [
+        { make: "Toyota", model: "Celica", price: 35000 },
+        { make: "Ford", model: "Mondeo", price: 32000 },
+        { make: "Porsche", model: "Boxter", price: 72000 },
+      ],
+    };
+  },
+};
+</script>
